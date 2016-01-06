34d3790b22b6c5f095e2dc2bfa061a6620d480d1
Lukas Benedix <lbenedix@l3q.de>
2016-01-06 16:34:37 +0100

    added pubDate

--- generate_feed.py
+++ generate_feed.py
@@ -1,4 +1,6 @@
 import dataset
+import time
+from email import utils
 from bs4 import BeautifulSoup
 
 feed = '<rss version="2.0">\n<channel>\n<title>Fefes Blog</title>\n<link>http://blog.fefe.de/</link>\n<description>Verschwörungen und Obskures aus aller Welt</description>\n<language>de</language>\n\n'
@@ -11,6 +13,7 @@ for item in all_items[:50]:
     feed += '<item>\n'
     feed += '<link>http://blog.fefe.de/?ts={}</link>\n'.format(item['item_id'])
     feed += '<guid>http://blog.fefe.de/?ts={}</guid>\n'.format(item['item_id'])
+    feed += '<pubDate>{}</pubDate>\n'.format(utils.formatdate(time.mktime(item['timestamp'].timetuple())))
 
     words = BeautifulSoup(item['text'], 'html5lib').text.split(' ')
     feed += '<title>{}'.format(' '.join(words[:10]))
