b20dda6cda236422c7b6b7a3fb7952eaa404bb67
lbenedix <lbenedix@nuc.fritz.box>
2020-07-22 00:45:20 +0200

    stuff

--- update.sh
+++ update.sh
@@ -6,10 +6,12 @@ pipenv run get_items
 pipenv run generate_feed
 
 if $(git diff --stat | grep "1 insertion(+), 1 deletion(-)"); then
-	git checkout feed.xml
+	echo "only one change"
+#	git checkout feed.xml
 else
-	git add .
-	git commit -m "new stuff"
-	git push
+	echo "real update"
+#	git add .
+#	git commit -m "new stuff"
+#	git push
 fi
 
