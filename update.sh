db94353c6c5a6bd658606081f4fcb7c9740771a4
lbenedix <lbenedix@nuc.fritz.box>
2020-07-22 00:43:27 +0200

    new stuff

--- update.sh
+++ update.sh
@@ -5,7 +5,11 @@ cd /home/lbenedix/Fefefeed
 pipenv run get_items
 pipenv run generate_feed
 
-git add .
-git commit -m "new stuff"
-git push
+if $(git diff --stat | grep "1 insertion(+), 1 deletion(-)"); then
+	git checkout feed.xml
+else
+	git add .
+	git commit -m "new stuff"
+	git push
+fi
 
