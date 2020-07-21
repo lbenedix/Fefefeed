f7f5ca17cc2893d07e0a969cf7ba84a5f8d59a00
lbenedix <lbenedix@nuc.fritz.box>
2020-07-22 00:52:46 +0200

    stuff

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ cd /home/lbenedix/Fefefeed
 pipenv run get_items
 pipenv run generate_feed
 
-git diff --stat | grep "feed.xml  |  2 +-"
+git diff --stat | grep feed.xml | grep "2 +-"
 if [ $? -eq 0 ]; then
 	echo OK, nothing new
 else
