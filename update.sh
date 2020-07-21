8e4bed2f2a91f21bce5407997804c15b306c1bb1
lbenedix <lbenedix@nuc.fritz.box>
2020-07-22 00:50:48 +0200

    stuff

--- update.sh
+++ update.sh
@@ -9,8 +9,8 @@ git diff --stat | grep "feed.xml  |  2 +-"
 if [ $? -eq 0 ]; then
 	echo OK, nothing new
 else
-	git add .
-	git commit -m "new stuff"
-	git push
+#	git add .
+#	git commit -m "new stuff"
+#	git push
 fi
 
