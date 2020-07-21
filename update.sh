5f6f1cdc8cf1b86f1eb89c6e4f3a24251cc8796c
lbenedix <lbenedix@nuc.fritz.box>
2020-07-22 00:53:49 +0200

    stuff

--- update.sh
+++ update.sh
@@ -8,10 +8,11 @@ pipenv run generate_feed
 git diff --stat | grep feed.xml | grep "2 +-"
 if [ $? -eq 0 ]; then
 	echo OK, nothing new
+	git checkout feed.xml
 else
 	echo "real update"
-#	git add .
-#	git commit -m "new stuff"
-#	git push
+	git add .
+	git commit -m "new stuff"
+	git push
 fi
 
